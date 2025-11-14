1ee9d7e376eb683f8641caa61128ee734e53e0de
lafisteri <nasulichanton@gmail.com>
2025-11-14 22:45:12 +0200

    optimise for logging

--- scripts/xvfb-start.sh
+++ scripts/xvfb-start.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
+
 Xvfb :99 -screen 0 1920x1080x24 -ac +extension RANDR +render -noreset &>/tmp/xvfb.log &
 sleep 0.5
+
 fluxbox &>/tmp/fluxbox.log &
 x11vnc -display :99 -nopw -forever -rfbport 5900 -shared -o /tmp/x11vnc.log &
-# noVNC на 7900
+
 if [ -d /usr/share/novnc ]; then
   websockify --web=/usr/share/novnc/ 7900 localhost:5900 &>/tmp/novnc.log &
 fi
-wait -n || true
+
+wait
